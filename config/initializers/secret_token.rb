19fe8085abf7f79660d66f64990baf52200e7314
Jamone Kelly <xg.monie089@gmail.com>
2013-07-21 04:01:59 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Anontwit::Application.config.secret_key_base = '84cec1b8b84ae55d150b9eb5d7b93cfa208ed955393ead6bdd1124a513bf63eef1094c71e69f5f27541a10849f2bfdafc0f8ac27e8bce71f7f1a424622a08af4'
